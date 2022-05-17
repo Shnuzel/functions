4543996c0fa044ec6fcc3d9c1b4b095e7ffe8fa5
unknown <ulsa22992299@hotmail.com>
2022-05-17 09:41:38 +0300

    functions

--- functions.js
+++ functions.js
@@ -5,9 +5,14 @@
 
 // console.log(SumNumbers(4,20))
 
+// 2
+// const SumNumbers = (num1,num2,num3) => {
+//     return num1+num2+num3;
+// }
 
-const SumNumbers = (num1,num2,num3) => {
-    return num1+num2+num3;
+// console.log(SumNumbers(9,8,9))
+
+const divideNum =(num1,num2) =>{
+    return num1/num2
 }
-
-console.log(SumNumbers(9,8,9))
+console.log(divideNum(8,4))
